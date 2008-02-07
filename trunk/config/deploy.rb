9d6b9f6d49544878aa3e6191e36d7fcff6461a13
trenton.cameron <trenton.cameron@34193435-9545-0410-99d1-f5f87e90116e>
2008-02-07 20:23:08 +0000

    added capistrano foo

---
+++ trunk/config/deploy.rb
@@ -0,0 +1,54 @@
+set :application, "oerfeeds"
+set :repository,  "http://oerfeeds.googlecode.com/svn/trunk/"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+ssh_options[:port] = 222
+
+role :app, "oerfeeds.info"
+role :web, "oerfeeds.info"
+role :db,  "oerfeeds.info", :primary => true
+
+
+desc "update vendor rails"
+task :update_rails do
+	for entry in Dir.entries("./vendor/rails")
+		if entry[/\REVISION_\d+/]
+			repository_revision  = entry.sub("REVISION_","")
+		end
+	end
+	puts repository_revision
+	sudo "svn up -r "+  repository_revision +" #{deploy_to}/shared/rails"
+end
+
+desc "create symlinks from rails dir into project"
+task :create_sym do
+	sudo "ln -nfs #{deploy_to}/shared/rails #{release_path}/vendor/rails"
+	sudo "chown -R mongrel:www  /var/www/oerfeeds"
+	sudo "chmod -R 775  /var/www/oerfeeds"
+end
+
+desc "tasks to run after checkout"
+task :after_update_code do
+	create_sym
+end
+desc "restart"
+task :restart do
+	sudo "#{deploy_to}/current/script/spin"
+end
+task :stop do
+	sudo "#{deploy_to}/current/script/process/reaper"
+end
+task :start do
+	sudo "#{deploy_to}/current/script/process/spawner"
+end
+task :tail_log, :roles => :app do
+stream "tail -f #{shared_path}/log/production.log"
+end
